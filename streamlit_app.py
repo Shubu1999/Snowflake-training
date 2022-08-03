0b48991728119265568f605a63bd39b42d9c6bf7
Shubhangi.khaparde <94069971+Shubu1999@users.noreply.github.com>
2022-08-03 19:36:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ streamlit.text('\N{bowl with spoon} Omega 3 and Blueberry Oatmeal')
 streamlit.text('\N{green salad} Kale, Spinach and Rocket Smoothie')
 streamlit.text('\N{rooster} Hard-Boiled Free-Range Egg')
 streamlit.text('\N{avocado}\N{bread} Avocado Toast')
+🥣	
