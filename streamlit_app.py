4adee771f60364b07de3618ce1fae0dfb9d66965
Shubhangi.khaparde <94069971+Shubu1999@users.noreply.github.com>
2022-08-03 19:44:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('\N{U+1F963} Breakfast Favorites')
+streamlit.header('🥑 Breakfast Favorites')
 streamlit.text('\N{bowl with spoon} Omega 3 and Blueberry Oatmeal')
 streamlit.text('\N{green salad} Kale, Spinach and Rocket Smoothie')
 streamlit.text('\N{rooster} Hard-Boiled Free-Range Egg')
