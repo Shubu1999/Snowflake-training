32ff792bf88e8c752aa47327aa7bbbd1b27970e4
Shubhangi.khaparde <94069971+Shubu1999@users.noreply.github.com>
2022-08-05 15:11:54 +0530

    Update streamlit_app.py
    using pandas to load a s3 bucket file

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,8 @@ streamlit.text('\N{rooster} Hard-Boiled Free-Range Egg')
 streamlit.text('\N{avocado} \N{bread} Avocado Toast')
 
 streamlit.header('\N{banana} \N{mango} Build Your Own Fruit Smoothie  \N{grapes}')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
